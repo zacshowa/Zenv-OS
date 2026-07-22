90f558d3912a0c730b070a807c74bd65874027fa
Zach Showalter <zacshowa@gmail.com>
2026-07-22 12:28:07 -0400

    update ai.nix to use codex and fence
    Claude code cannot be used on the free teir, and I will not spend my own
    money on a claude subscription while I have no income lmao.

--- ai.nix
+++ ai.nix
@@ -1,11 +1,19 @@
 {
-inputs,
-pkgs,
-...
+  inputs,
+  pkgs,
+  ...
 }:
 
 {
-	  environment.systemPackages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
-			claude-sandbox
-		];
+  nix.settings = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [
+      "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g="
+    ];
+  };
+
+  environment.systemPackages = with inputs.llm-agents.packages.${pkgs.stdenv.hostPlatform.system}; [
+    codex
+    fence
+  ];
 }
